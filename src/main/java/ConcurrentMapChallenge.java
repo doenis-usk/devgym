e6db0cf277e517253f3aedd6b518c59420cc4d19
doenis-usk <yusuke@yamamotoyuusukenoMacBook-Pro.local>
2020-01-04 10:56:59 +0900

    initial commit

---
+++ src/main/java/ConcurrentMapChallenge.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ConcurrentMapChallenge {
+
+    public static void main(String... doYourBest) {
+        ConcurrentMap<String, Object> cache = new ConcurrentHashMap<>();
+        cache.put("111", new Double(5));
+        cache.putIfAbsent("111", "LOL");
+        cache.put("111", new Integer(4));
+        cache.put("222", new Integer(4));
+
+        System.out.println(cache);
+    }
+}
